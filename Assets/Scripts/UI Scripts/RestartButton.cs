5a91fd9798d05fb9300eb4d24a0de07aa3bfab99
PrebenSivhat <martinkraghhansen@gmail.com>
2017-01-16 23:37:14 +0100

    Ui way better! Not 100% done though

---
+++ Assets/Scripts/UI Scripts/RestartButton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RestartButton : MonoBehaviour {
+
+	
+
+	void Start ()
+	{
+	   
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (ScoreController.PlayersAlive == true)
+	    {
+            Destroy(gameObject);
+            
+	    }
+	}
+}
